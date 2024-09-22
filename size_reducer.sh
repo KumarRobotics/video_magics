2c03815b8db40e8531778c66545912f101648a48
Fernando Cladera <fcladera@fcladera.com>
2024-09-22 11:08:35 -0400

    Add parameters to configure. Add support for h265.

--- size_reducer.sh
+++ size_reducer.sh
@@ -2,6 +2,14 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 
+########################################
+# Parameters
+# Modify these to fit your needs
+initial_quality=5000k # Initial quality to estimate the output size
+target_size=18000000 # Target size in bytes. Make it slightly smaller than 20*2**6
+encoder=x264 # Either x264 or x265
+########################################
+
 # Check for input arguments. Only a single argument is allowed, the file name
 if [ "$#" -ne 1 ]; then
   echo -e "${RED}Illegal number of parameters"
@@ -21,44 +29,50 @@ if [ ! -f "$input" ]; then
 fi
 
 # Set the initial quality to 950k
-quality=5000k
+quality=$initial_quality
 
-# Print in yellow input and quality
+# Print the input and quality
 echo -e "${YELLOW}Input: $input"
 echo -e "${YELLOW}Quality: $quality"
 
-# H265
-# ffmpeg -y -i "$input" -c:v libx265 -b:v "$quality" -x265-params pass=1 -an -f
-# null /dev/null && ffmpeg -i "$input" -c:v libx265 -b:v "$quality" -x265-params
-# pass=2 -c:a aac -b:a 64k "$output"
-#
-# H264
-ffmpeg -hide_banner -loglevel warning -y -i "$input" -c:v libx264 \
-  -b:v "$quality" -x264-params pass=1 -an -f null /dev/null && \
-  ffmpeg -hide_banner -loglevel warning -y -i "$input" -c:v libx264 \
-  -b:v "$quality" -x264-params pass=2 -c:a aac -b:a 64k "$output"
+if [ "$encoder" == "x265" ]; then
+  echo -e "${YELLOW}Encoder: x265"
+  libx="-c:v libx265"
+  xparams="-x265-params"
+elif [ "$encoder" == "x264" ]; then
+  echo -e "${YELLOW}Encoder: x264"
+  libx="-c:v libx264"
+  xparams="-x264-params"
+else
+  echo -e "${RED}Encoder $encoder not supported"
+  exit 1
+fi
 
-# Get the size of the output file and modify the quality accordingly to fit in
-# 20 MB
+# Perform first compression
+echo -e "${GREEN}Performing first compression"
+ffmpeg -hide_banner -loglevel warning -y -i "$input" $libx -b:v "$quality" \
+  $xparams pass=1 -an -f null /dev/null && \
+  ffmpeg -hide_banner -loglevel warning -y -i "$input" $libx -b:v "$quality" \
+  $xparams pass=2 -c:a aac -b:a 64k "$output"
+
+# Get the size of the output file and modify the quality accordingly to fit
 output_size=$(stat -c%s "$output")
-multiplier=$(echo "scale=2; $output_size / 19000000" | bc)
-# don't forget to remove the k from quality
+multiplier=$(echo "scale=2; $output_size / $target_size" | bc)
+# Remove the k from quality
 quality_without_k=${quality%k}
 new_quality=$(echo "scale=0; $quality_without_k / $multiplier" | bc)
 
 # Add k to new quality
 new_quality=${new_quality}k
-# Print new quality in green
 echo -e "${GREEN}New quality: $new_quality"
 
 # Compress again
-ffmpeg -hide_banner -loglevel warning -y -i "$input" -c:v libx264 \
-  -b:v "$new_quality" -x264-params pass=1 -an -f null /dev/null && \
-  ffmpeg -hide_banner -loglevel warning -y -i "$input" -c:v libx264 \
-  -b:v "$new_quality" -x264-params pass=2 -c:a aac -b:a 64k "$output"
+echo -e "${GREEN}Performing second compression"
+ffmpeg -hide_banner -loglevel warning -y -i "$input" $libx -b:v "$new_quality" \
+  $xparams pass=1 -an -f null /dev/null && \
+  ffmpeg -hide_banner -loglevel warning -y -i "$input" $libx -b:v "$new_quality" \
+  $xparams pass=2 -c:a aac -b:a 64k "$output"
 
 # Get final size
 output_size=$(stat -c%s "$output")
-
-# print in green
 echo -e "${GREEN}Final size: $output_size"
